b276a04e307ea73bfbc18794ae36e5a36e15e810
atanasster <atanasster@gmail.com>
2020-02-08 18:47:59 -0500

    remove addon-controls

--- addons/controls/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { extractSmartControls } from './smartControls';
--- addons/controls/src/shared.ts
+++
@@ -1,4 +0,0 @@
-// addons, panels and events get unique names using a prefix
-export const PARAM_KEY = 'addon-controls';
-export const ADDON_ID = 'storybookjs/addon-controls';
-export const PANEL_ID = `${ADDON_ID}/panel`;
