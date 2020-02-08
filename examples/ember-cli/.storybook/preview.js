bad883ecea393ad4061c8f345958cfe283deebf9
atanasster <atanasster@gmail.com>
2020-02-08 18:38:40 -0500

    cleanup examples more

--- examples/ember-cli/.storybook/preview.js
+++ examples/ember-cli/.storybook/preview.js
@@ -1,5 +1,6 @@
 import { addParameters, addDecorator } from '@storybook/ember';
 import { setJSONDoc } from '@storybook/addon-docs/ember';
+// eslint-disable-next-line import/no-unresolved
 import docJson from '../ember-output/storybook-docgen/index.json';
 
 setJSONDoc(docJson);
--- examples/official-storybook/components/BaseButton.js
+++ examples/official-storybook/components/BaseButton.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { lighten } from 'polished';
 import PropTypes from 'prop-types';
 
 /** BaseButton component description imported from comments inside the component file */
-const BaseButton = ({ disabled, label, onClick, style, backgroundColor, color, type, padding }) => (
-  // eslint-disable-next-line react/button-has-type
-  <button
-    type={type}
-    disabled={disabled}
-    onClick={onClick}
-    style={{ ...style, backgroundColor, color: lighten(disabled ? 0.3 : 0, color), padding }}
-  >
+const BaseButton = ({ disabled, label, onClick, style }) => (
+  <button type="button" disabled={disabled} onClick={onClick} style={style}>
     {label}
   </button>
 );
@@ -19,10 +12,6 @@ BaseButton.defaultProps = {
   disabled: false,
   onClick: () => {},
   style: {},
-  backgroundColor: '#fefefe',
-  color: 'black',
-  type: 'button',
-  padding: 5,
 };
 
 BaseButton.propTypes = {
@@ -34,18 +23,6 @@ BaseButton.propTypes = {
   onClick: PropTypes.func,
   /** Custom styles */
   style: PropTypes.shape({}),
-
-  /** Background color */
-  backgroundColor: PropTypes.string,
-
-  /** Text color, default black */
-  color: PropTypes.string,
-
-  /** Button type */
-  type: PropTypes.oneOf(['button', 'reset', 'submit']),
-
-  /** Numeric  field type */
-  padding: PropTypes.number,
 };
 
 export default BaseButton;
