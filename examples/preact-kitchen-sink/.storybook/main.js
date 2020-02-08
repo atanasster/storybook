4685cd5e18ec59bc2142786730a25f8f537807fb
atanasster <atanasster@gmail.com>
2020-02-08 18:12:51 -0500

    remove preact controls stories

--- examples/preact-kitchen-sink/.storybook/main.js
+++ examples/preact-kitchen-sink/.storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     '@storybook/addon-storysource',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@component-controls/storybook',
     '@storybook/addon-knobs',
     '@storybook/addon-viewport',
     '@storybook/addon-options',
